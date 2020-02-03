dd8b9da26e4d3b25a91e0913e7f1489ad5ae6d3c
Salma Samir <salmashoair@gmail.com>
2020-02-03 17:29:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ function dynamicallyLoadScript() {
 
     var script1 = document.createElement("script");  // create a script DOM node
     script1.src = "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js";  // set its src to the provided URL
+    // script1.integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==";
     script1.crossorigin= "";
-    document.body.appendChild(script1); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+    document.head.appendChild(script1); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 
     var script2 = document.createElement("script");
     script2.src = "https://unpkg.com/esri-leaflet@2.3.2/dist/esri-leaflet.js";
     script2.crossorigin= "";
-    document.body.appendChild(script2);
+    document.head.appendChild(script2);
 
-    var scrip3 = document.createElement("script");
-    scrip3.src = "https://unpkg.com/esri-leaflet-geocoder@2.3.2/dist/esri-leaflet-geocoder.js";  
-    scrip3.crossorigin= "";
-    document.body.appendChild(scrip3);
+    var script3 = document.createElement("script");
+    script3.src = "https://unpkg.com/esri-leaflet-geocoder@2.3.2/dist/esri-leaflet-geocoder.js";  
+    script3.crossorigin= "";
+    document.head.appendChild(script3);
 }
 
-dynamicallyLoadScript();
+window.onload=dynamicallyLoadScript;
+
 
 
 {/* <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
