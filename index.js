3dc0e5a58fcea06dba7b218fc2f692908f0a63d8
Salma Samir <salmashoair@gmail.com>
2020-02-03 15:51:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function dynamicallyLoadScript() {    
+
+    var script1 = document.createElement("script");  // create a script DOM node
+    script1.src = "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js";  // set its src to the provided URL
+    script1.crossorigin= "";
+    document.body.appendChild(script1); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+
+    var script2 = document.createElement("script");
+    script2.src = "https://unpkg.com/esri-leaflet@2.3.2/dist/esri-leaflet.js";
+    script2.crossorigin= "";
+    document.body.appendChild(script2);
+
+    var scrip3 = document.createElement("script");
+    scrip3.src = "https://unpkg.com/esri-leaflet-geocoder@2.3.2/dist/esri-leaflet-geocoder.js";  
+    scrip3.crossorigin= "";
+    document.body.appendChild(scrip3);
+}
+
+dynamicallyLoadScript();
+
+
+{/* <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+crossorigin=""></script>
+
+
+<script src="https://unpkg.com/esri-leaflet@2.3.2/dist/esri-leaflet.js"
+integrity="sha512-6LVib9wGnqVKIClCduEwsCub7iauLXpwrd5njR2J507m3A2a4HXJDLMiSZzjcksag3UluIfuW1KzuWVI5n/cuQ=="
+crossorigin=""></script>
+
+
+
+<script src="https://unpkg.com/esri-leaflet-geocoder@2.3.2/dist/esri-leaflet-geocoder.js"
+  integrity="sha512-8twnXcrOGP3WfMvjB0jS5pNigFuIWj4ALwWEgxhZ+mxvjF5/FBPVd5uAxqT8dd2kUmTVK9+yQJ4CmTmSg/sXAQ=="
+  crossorigin=""></script> */}
